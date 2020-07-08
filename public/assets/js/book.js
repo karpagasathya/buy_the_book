c64e21a4e29a5dff8abf95eabd5b946f93eba17f
Jacob Kyle <jkthomps21@gmail.com>
2020-07-08 18:19:44 -0500

    Added public folder with JS, CSS, and IMG folders. Created index and main handlebars.

---
+++ public/assets/js/book.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+    $("#").show();
+    $("#").hide();
+    $("#").hide();
+}
+
+$("#button").on("click", function (){
+    $("#").show();
+    $("#").hide();
+    $("#").hide();
+});
+
+$("#button").on("click", function (){
+    $("#").hide();
+    $("#").show();
+    $("#").hide();
+});
+
+$("#button").on("click", function (){
+    $("#").hide();
+    $("#").hide();
+    $("#").show();
+});

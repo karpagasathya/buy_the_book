21d73ab27335cc64217b5fb051faa8342bccf581
sathya <karpagasathya@gmail.com>
2020-07-07 23:06:03 -0500

    server file

---
+++ server.js
@@ -0,0 +1,28 @@
+let express = require("express");
+
+let PORT = process.env.PORT || 8080;
+
+let app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+let exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+require("./routes/api-routes.js")(app);
+
+
+
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
+});

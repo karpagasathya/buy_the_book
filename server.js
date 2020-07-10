6cdfe186e59a2806f7e52e998fff1ea459d23408
sathya <karpagasathya@gmail.com>
2020-07-09 21:07:56 -0500

    removed force:true in db.sequelize function

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/", require("./routes/html-routes"));
 
 
 
-db.sequelize.sync({ force: true }).then(function () {
+db.sequelize.sync().then(function () {
   app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });

48e646c988deda229b73af0f99a6d885d3505a5b
sathya <karpagasathya@gmail.com>
2020-07-11 02:44:46 -0500

    refactored html-route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,58 +3,18 @@ let router = express.Router();
 
 const db = require("../models");
 
-// //find all books
-// router.get("/", (req, res) =>
-//   db.Book
-//     .findAll()
-//     .then((books) => {
-//       console.log(books);
-//       res.render("index", {
-//        Book: books,
-//       });
-//     })
-//     .catch((err) => console.log(err))
-// );
-
-router.get("/", (req, res) =>
-    db.Book.findAll({
-        attributes: ["genre"],
-        group: ["genre"],
-    }).then((books) => {
-        let categories = books.map((book) => book.genre)
-        console.log(categories);
+router.get("/", (req, res) => {
+    const allBooks = db.Book.findAll();
+    const distinctCategory = db.Book.aggregate("genre", "DISTINCT", { plain: false });
+    
+    Promise.all([allBooks, distinctCategory])
+        .then((responses) => {
         res.render("index", {
-            category: categories
-        })
-    })
-);
+          top9Books: responses[0],
+          category: responses[1],
+        });
+      })
+      .catch((err) => console.log(err));
+});
 
-// router.get("/", (req, res) =>
-//   db.Book.findAll({
-//     attributes: ["genre"],
-//     group: ["genre"],
-//   }).then((books) => {
-//     // let categories = books.map((book) => book.genre)
-//     // console.log(categories);
-//     res.render("index", {
-//       Category: books.map((book) => book.genre),
-//     });
-//   })
-// );
-
-
-
-// //SHOW- Show info about one book
-// router.get("/:id", (req, res)=> {
-  
-//   db.Book.findById(req.params.id)
-//     .exec(function (err, foundBook) {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         res.render("books/show", { book: foundBook });
-//       }
-//     });
-// });
-
-module.exports = router;
+(module.exports = router);

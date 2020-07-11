77787ffd2e781e40f41a5bf79f7125dac2118f83
sathya <karpagasathya@gmail.com>
2020-07-10 23:19:34 -0500

    trying to get the genre in index page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,28 +3,58 @@ let router = express.Router();
 
 const db = require("../models");
 
-
-
-
+// //find all books
+// router.get("/", (req, res) =>
+//   db.Book
+//     .findAll()
+//     .then((books) => {
+//       console.log(books);
+//       res.render("index", {
+//        Book: books,
+//       });
+//     })
+//     .catch((err) => console.log(err))
+// );
 
 router.get("/", (req, res) =>
-  db.books
-    .findAll()
-    .then((books) => {
-      console.log(books);
-      res.render("index", {
-        index: books,
-      });
+    db.Book.findAll({
+        attributes: ["genre"],
+        group: ["genre"],
+    }).then((books) => {
+        let categories = books.map((book) => book.genre)
+        console.log(categories);
+        res.render("index", {
+            category: categories
+        })
     })
-    .catch((err) => console.log(err))
 );
 
+// router.get("/", (req, res) =>
+//   db.Book.findAll({
+//     attributes: ["genre"],
+//     group: ["genre"],
+//   }).then((books) => {
+//     // let categories = books.map((book) => book.genre)
+//     // console.log(categories);
+//     res.render("index", {
+//       Category: books.map((book) => book.genre),
+//     });
+//   })
+// );
 
-// router.get('/', (req, res) => {
-//     db.books.findAll({})
-//         .then(data => {
-//         res.json(data)
-//     })
-// })
+
+
+// //SHOW- Show info about one book
+// router.get("/:id", (req, res)=> {
+  
+//   db.Book.findById(req.params.id)
+//     .exec(function (err, foundBook) {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.render("books/show", { book: foundBook });
+//       }
+//     });
+// });
 
 module.exports = router;

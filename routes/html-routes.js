a12a90229eb4703fed60b11592a59794ae3c4a68
sathya <karpagasathya@gmail.com>
2020-07-12 03:12:18 -0500

    added route for category

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,20 +1,64 @@
 let express = require("express");
 let router = express.Router();
+var Sequelize = require("sequelize");
 
 const db = require("../models");
 
 router.get("/", (req, res) => {
-    const allBooks = db.Book.findAll();
+    // fetching random 9 books to display in index page
+    const allBooks = db.Book.findAll({ limit: 9, order: Sequelize.literal("rand()") });
     const distinctCategory = db.Book.aggregate("genre", "DISTINCT", { plain: false });
     
     Promise.all([allBooks, distinctCategory])
         .then((responses) => {
         res.render("index", {
-          top9Books: responses[0],
-          category: responses[1],
+          books: responses[0],
+          categories: responses[1],
         });
       })
       .catch((err) => console.log(err));
+});
+
+router.post("/category/:categoryName", (req, res) => {
+  const booksByCategory = db.Book.findAll({
+    where: {
+      genre: req.body.genre,
+    },
+  });
+
+  const distinctCategory = db.Book.aggregate("genre", "DISTINCT", { plain: false });
+
+  Promise.all([booksByCategory, distinctCategory])
+    .then((responses) => {
+      res.render("category", {
+        books: responses[0],
+        categories: responses[1],
+      });
+    })
+    .catch((err) => console.log(err));
+});
+
+router.get("/cart", (req, res) => {
+  const cartItems = db.Cart.findAll({
+    include: [
+      {
+        model: db.Book,
+        through: {
+          model: db.CartBook,
+        },
+      },
+    ],
+  });
+  // const distinctCategory = db.Book.aggregate("genre", "DISTINCT", { plain: false });
+
+  Promise.all([cartItems])
+    .then((responses) => {
+      console.log(responses[0])
+      res.render("cart", {
+        cart: responses[0],
+      });
+    })
+    .catch((err) => console.log(err));
 });
 
 (module.exports = router);
